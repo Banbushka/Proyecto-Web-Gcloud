656af11af02dd09d79cc3e25e2b937bc1e3c7707
DiegoCosaVelarde <crow262728@gmail.com>
2023-06-18 10:24:28 +0200

    funcion

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,9 +1,27 @@
+from google.cloud import storage
+from google.cloud import firestore
+import json
+
 def function_uwee(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    print("HOLAAA FUNCIONAAA VAMOSSS!!!!")
-    file = event
-    print(f"Processing file: {file['name']}.")
+
+        # Obtener el nombre del archivo JSON creado en Cloud Storage
+        bucket_name = 'uwee'  # Cambiar el nombre del bucket aquí
+        file_name = event['name']
+
+        # Crear una instancia del cliente de Cloud Storage
+        storage_client = storage.Client()
+
+        # Obtener el objeto JSON desde Cloud Storage
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(file_name)
+        json_data = blob.download_as_text()
+
+        # Convertir el JSON a un diccionario de Python
+        data = json.loads(json_data)
+
+        # Guardar los datos en Cloud Firestore
+        firestore_client = firestore.Client()
+        collection = firestore_client.collection('users')
+        collection.document().set(data)
+
+        return 'Datos guardados en Cloud Firestore'
